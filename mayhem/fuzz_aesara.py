b395ec594b1ea9ab299b7d5dcebf5e0efe980c9c
Vanya <user@orp.local>
2022-12-31 20:54:42 -0500

    updated harness

--- mayhem/fuzz_aesara.py
+++ mayhem/fuzz_aesara.py
@@ -11,17 +11,17 @@ with atheris.instrument_imports():
 def test_input(input_bytes):
     fdp = atheris.FuzzedDataProvider(input_bytes)
     input_string = fdp.ConsumeUnicodeNoSurrogates(sys.maxsize)
-    input_str_w_surrogates = fdp.ConsumeUnicode(sys.maxsize)
-    input_int = fdp.ConsumeInt(sys.maxsize)
+    # input_str_w_surrogates = fdp.ConsumeUnicode(sys.maxsize)
+    # input_int = fdp.ConsumeInt(sys.maxsize)
     try:
-        a = at.dscalar(input_string)
-        f_c = aesara.function([input_string, input_string], input_string)
-        dc = aesara.grad(input_string, input_string)
-        f_dc = aesara.function([input_string, input_string], input_string)
-        v = at.vector(input_string)
-        M = at.matrix(input_string)
-        aesara.dprint(input_string)
-        f_d = aesara.function([input_string, input_string, input_string], input_string)
+        # a = at.dscalar(input_string)
+        # f_c = aesara.function([input_string, input_string], input_string)
+        # dc = aesara.grad(input_string, input_string)
+        # f_dc = aesara.function([input_string, input_string], input_string)
+        # v = at.vector(input_string)
+        # M = at.matrix(input_string)
+        # aesara.dprint(input_string)
+        # f_d = aesara.function([input_string, input_string, input_string], input_string)
         aesara.dprint(input_string)
     except TypeError:
         pass
